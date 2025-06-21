21eee2d6055cc84c12ee3b824b5ba4ce568a6ea8
SWAPNIL2804RAJ <124730254+SWAPNIL2804RAJ@users.noreply.github.com>
2025-06-21 22:59:39 +0530

    updated backend index.js

--- server/index.js
+++ server/index.js
@@ -6,7 +6,11 @@ require('dotenv').config();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors(
+  {
+    origin: 'https://vercel.com/swapnil2804rajs-projects/multi-channel-order-sync-tracker', // Replace with your frontend URL
+  }
+));
 app.use(express.json());
 
 // Routes
