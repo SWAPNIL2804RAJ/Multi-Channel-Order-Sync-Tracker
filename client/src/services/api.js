4d25ef1d8865d8347094f1eb236e31352a597542
SWAPNIL2804RAJ <124730254+SWAPNIL2804RAJ@users.noreply.github.com>
2025-06-21 20:57:54 +0530

    Update backend Url

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000/api',        //Render backend URL Or // MongoDB Atlas API base URL
+  baseURL: 'https://multi-channel-order-sync-tracker-avae.onrender.com' || 'http://localhost:5000/api',        //Render backend URL Or // MongoDB Atlas API base URL
 });
 
 export default API;
